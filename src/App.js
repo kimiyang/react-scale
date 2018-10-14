ae312d92ab5b7fcc9b7646de49bf51547a6e7230
YangWenhua <wenhuakimi@gmail.com>
2018-10-14 18:19:23 +0800

    support customized indicator

--- src/App.js
+++ src/App.js
@@ -2,8 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 import SlidingScale from './sliding_scale';
 
+const styles = {
+  amount: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#0093D1'
+  },
+  indicatorContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+  },
+  indicator: {
+    width: 2,
+    height: 50,
+    background: 'red'
+  }
+}
+
 class App extends Component {
   render() {
+    const renderAmount = (amount) => <div style = {styles.amount}>
+      { amount }%
+    </div>;
+
+    const renderIndicator = () => <div style={styles.indicatorContainer}>
+      <div style={styles.indicator}></div>
+    </div>
+
     const config = {
       minValue: 1,
       maxValue: 20,
@@ -16,16 +45,16 @@ class App extends Component {
       subSegments: 10,
       scaleHeight: 20,
       subScaleHeight: 10,
-      scaleFontColor: '#2A2A2A',
-      scaleSelectedFontColor: '#0093D1',
-      labelColor: '#2A2A2A',
-      labelSelectedColor: '#0093D1',
-      indactorColor: '#0093D1',
-      amountSuffix: '%',
-      amountStyle: {
-        fontSize: 30,
-        fontWeight: 'bold',
-      }
+      scaleStyle: {
+        scaleFontSize: 16,
+        scaleFont: 'PingFang SC',
+        scaleFontColor: '#2A2A2A',
+        scaleSelectedFontColor: '#0093D1',
+        labelColor: '#2A2A2A',
+        labelSelectedColor: '#0093D1',
+      },
+      renderAmount,
+      renderIndicator
     };
     return (
       <div className="App">
--- src/sliding_scale/index.js
+++ src/sliding_scale/index.js
@@ -63,11 +63,7 @@ class SlidingScale extends PureComponent {
   }
 
   drawScale() {
-    // console.log('drawing', this.position, this.lastPosition);
-    // console.log('is moving: ', this.moving);
-    if (Math.abs(this.position - this.lastPosition) < 0.1) {
-
-    } else {
+    if (Math.abs(this.position - this.lastPosition) >= 0.1) {
       const { ctx, position, STEP_WIDTH } = this;
       const { defaultValue,
         minValue,
@@ -78,20 +74,22 @@ class SlidingScale extends PureComponent {
         subSegments,
         subScaleHeight,
         scaleHeight,
+        scaleStyle,
+        renderIndicator
+      } = this.props.config || {};
+      const { 
         scaleFontSize = 16,
         scaleFont = 'PingFang SC',
         scaleFontColor = '#000000',
         scaleSelectedFontColor = '#000000',
         labelColor = '#000000',
-        labelSelectedColor = '#000000',
-        drawIndicator = true,
-        indactorColor = '#000000',
-      } = this.props.config || {};
+        labelSelectedColor = '#000000' 
+      } = scaleStyle || {};
+
       this.lastPosition = position;
       const left = Math.floor((position - canvasWidth / 2) / STEP_WIDTH ) * step + defaultValue;
       const right = Math.ceil((position + canvasWidth / 2) / STEP_WIDTH ) * step + defaultValue;
       ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-      console.log(left, right, step, STEP_WIDTH, canvasWidth);
       // draw unselectable scale
       ctx.font = `${scaleFontSize}px "${scaleFont}"`;
       ctx.fillStyle = `${scaleFontColor}`;
@@ -109,20 +107,18 @@ class SlidingScale extends PureComponent {
           ctx.fillStyle = `${scaleSelectedFontColor}`;
         }
         ctx.fillRect(posX - 0.5, 30, 1, scaleHeight);
-        console.log('amount:', this.state.amount, i, posX - 0.5)
 
         // draw numbers
         ctx.fillStyle = `${labelColor}`;
         if (this.state.amount == i) {
-          console.log('selected label', i);
           ctx.fillStyle = `${labelSelectedColor}`;
         }
         ctx.fillText(`${i}`, posX - ctx.measureText(i).width / 2, 30 + scaleHeight * 2);
       }
-      if (drawIndicator) {
+      if (!renderIndicator) {
         const posX = canvasWidth / 2;
-        ctx.fillStyle = `${indactorColor}`;
-        ctx.fillRect(posX - 1, 0, 2, 40);
+        ctx.fillStyle = '#111111';
+        ctx.fillRect(posX - 1, 0, 2, 30 + scaleHeight);
       }
 
     }
@@ -143,7 +139,6 @@ class SlidingScale extends PureComponent {
     const dock_step_wdith = isDockToSub ? this.SUB_STEP_WIDTH : this.STEP_WIDTH;
     const dock_Step = isDockToSub ? step / subSegments : step;
     let nextAmount = Math.round(this.position / dock_step_wdith) * dock_Step + defaultValue;
-    console.log(this.state.amount, nextAmount, nextPos, this.position);
     if (this.state.amount !== nextAmount) {
       if (isDockToSub) {
         nextAmount = nextAmount.toFixed(1);
@@ -155,7 +150,6 @@ class SlidingScale extends PureComponent {
   }
 
   brake(dt) {
-    // console.log('braking');
     const now = new Date();
     const { minAcceleration = 1000 } = this.props.config;
     if (this.position === this.minPos - this.STEP_WIDTH || this.position === this.maxPos + this.STEP_WIDTH) {
@@ -167,7 +161,6 @@ class SlidingScale extends PureComponent {
     const acceleration = Math.max(Math.abs(this.prevSpeed * 4), minAcceleration);
     const deltaSpeed = acceleration * seconds;
     const nextSpeed = this.prevSpeed > 0 ? this.prevSpeed - deltaSpeed : this.prevSpeed + deltaSpeed;
-    // console.log(this.prevSpeed, nextSpeed, this.prevSpeed);
     if (nextSpeed * this.prevSpeed > 0) {
       this.moveScale((this.prevSpeed + nextSpeed) * seconds / 2);
       this.prevSpeed = nextSpeed;
@@ -181,20 +174,17 @@ class SlidingScale extends PureComponent {
   }
 
   dock(dt) {
-    // console.log('docking', this.prevSpeed, this.minPos, this.maxPos);
     const { minAcceleration = 1000, dockToSub = false } = this.props.config;
     
     const dock_step_wdith = dockToSub ? this.SUB_STEP_WIDTH :this.STEP_WIDTH;
     let dockPos = Math.round(this.position / dock_step_wdith) * dock_step_wdith;
     dockPos = Math.max(Math.min(this.maxPos, dockPos), this.minPos);
-    // console.log('docking pos', dockPos, this.position);
     const dist = this.position - dockPos;
     const now = new Date();
     const seconds = (now - dt) / 1000;
     const nextSpeed = this.prevSpeed + minAcceleration * seconds;
     let deltaPos = (this.prevSpeed + nextSpeed) * seconds / 2;
     deltaPos = dist > 0 ? deltaPos : -deltaPos;
-    // console.log(dockPos, deltaPos, dist, this.prevSpeed, nextSpeed);
     this.dockToken = doAnimation(() => {
       if (Math.abs(deltaPos) > Math.abs(dist)) {
         this.moveScale(dist);
@@ -208,10 +198,8 @@ class SlidingScale extends PureComponent {
   }
  
   onTouchStart(e) {
-    // console.log('touch start', e);
     this.prevX = e.nativeEvent.touches[0].clientX;
     this.prevTimeStamp = e.nativeEvent.timeStamp;
-    // console.log(this.prevX, this.prevTimeStamp);
     this.brakeToke && cancelAnimation(this.brakeToke);
     this.dockToken && cancelAnimation(this.dockToken);
     this.prevSpeed = 0;
@@ -222,10 +210,8 @@ class SlidingScale extends PureComponent {
   onTouchEnd(e) {
     this.prevX = null;
     this.brake(new Date());
-    // console.log('touch end', e);
   }
   onTouchMove(e) {
-    // console.log('touch move', this.prevX);
     if (this.prevX) {
       const nextX = e.nativeEvent.touches[0].clientX;
       const nextTimeStamp = e.nativeEvent.timeStamp;
@@ -234,20 +220,26 @@ class SlidingScale extends PureComponent {
       this.prevX = nextX;
       this.prevTimeStamp = nextTimeStamp;
       this.moving = true;
-      // console.log('in touch move', deltaX);
       this.moveScale(deltaX);
     }
   }
 
   render() {
-    const { canvasWidth, canvasHeight, amountStyle, amountSuffix } = this.props.config || {};
+    const { canvasWidth, canvasHeight, renderAmount, renderIndicator } = this.props.config || {};
+    const amountNode = renderAmount && typeof renderAmount === "function" ? renderAmount(this.state.amount) : null;
+    const indicatorNode = renderIndicator && typeof renderIndicator === "function" ? renderIndicator() : null;
     return (
       <div className="sliding-scale"
         onTouchStart = {this.onTouchStart}
         onTouchMove = {this.onTouchMove}
         onTouchEnd = {this.onTouchEnd}
       >
-        <div className='amount' style={amountStyle}>{this.state.amount}{amountSuffix}</div>
+        {
+          amountNode || <div className='amount'>{this.state.amount}</div>
+        }
+        {
+          indicatorNode
+        }
         <canvas key='scale' width={canvasWidth} height={canvasHeight} ref={
           el => {
             if(el && !this.ctx) {
